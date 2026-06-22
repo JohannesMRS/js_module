bc452d35f118b552b5b9203d3deb9ff4108f0b7b
Johannes <mariojr8304@gmail.com>
2026-06-22 10:03:18 +0700

    Feat: Adding Style.css

--- 02-dom-manipulation/01-dom-selection/script.js
+++ 02-dom-manipulation/01-dom-selection/script.js
@@ -2,16 +2,75 @@ const judul = document.getElementById("nama-kelas");
 const btn = document.getElementsByTagName("button");
 const btnConvert = [...btn];
 
-const li = document.querySelectorAll(".lulus");
+const btnShow = document.getElementById("tampilkan");
+const listData = document.getElementById("listData");
+const showLulus = document.getElementById("showLulus");
 
-judul.style.color = "green";
+const databaseMahasiswa = [
+  {
+    nama: "rian hidayat",
+    ipk: 3.8,
+    isOrganisasi: true,
+    nim: "240101",
+    status: "Lulus",
+  },
+  {
+    nama: "sari utami",
+    ipk: 3.2,
+    isOrganisasi: true,
+    nim: "240102",
+    status: "Belum Lulus",
+  },
+  {
+    nama: "toni perkasa",
+    ipk: 3.9,
+    isOrganisasi: false,
+    nim: "240103",
+    status: "Lulus",
+  },
+  {
+    nama: "gita lestari",
+    ipk: 3.7,
+    isOrganisasi: true,
+    nim: "240104",
+    status: "Belum Lulus",
+  },
+  {
+    nama: "budi santoso",
+    ipk: 3.4,
+    isOrganisasi: false,
+    nim: "240105",
+    status: "Belum Lulus",
+  },
+];
 
 judul.textContent = "Dashboard Evaluasi Polmed";
 
-btnConvert.forEach((item) => {
-  item.style.fontWeight = "bold";
+// btnShow.addEventListener("click", () => {
+//   databaseMahasiswa.forEach((item) => {
+//     document.write("Nama: ", item.nama, "<br>");
+//     document.write("Nama: ", item.ipk, "<br>");
+//     document.write("Nama: ", item.isOrganisasi, "<br>");
+//     document.write("Nama: ", item.nim, "<br>");
+//     document.write("----------------------------------------");
+//     document.write("</br>");
+//   });
+// });
+
+let gabungan = "";
+
+btnShow.addEventListener("click", () => {
+  listData.innerHTML = "";
+  databaseMahasiswa.forEach((item) => {
+    listData.innerHTML += `<p>Nama:${item.nama}<br>IPK: ${item.ipk}<br>Status: ${item.status}</p>`;
+  });
 });
 
-li.forEach((item) => {
-  item.style.backgroundColor = "yellow";
+showLulus.addEventListener("click", () => {
+  listData.innerHTML = "";
+  databaseMahasiswa
+    .filter((item) => item.status === "Lulus")
+    .forEach((item) => {
+      listData.innerHTML += `<p>Nama: ${item.nama} <br>IPK: ${item.ipk} <br> Status: ${item.status}</p>`;
+    });
 });
