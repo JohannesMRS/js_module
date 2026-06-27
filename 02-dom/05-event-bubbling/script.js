7233d6466d0a11ef60d92b632a3911d0accabda7
JohannesMRS <mariojr8304@gmail.com>
2026-06-27 22:14:35 +0700

    Feat: Adding soal.html file

--- 02-dom/05-event-bubbling/script.js
+++ 02-dom/05-event-bubbling/script.js
@@ -7,16 +7,30 @@
 //     }
 // })
 
-const containerMeja = document.getElementById('container-meja');
+// const containerMeja = document.getElementById('container-meja');
 
-containerMeja.addEventListener('click', (e)=>{
-    if(e.target.classList.contains('btn-kosongkan')){
-        const dataMeja = e.target.getAttribute('data-meja');
+// containerMeja.addEventListener('click', (e)=>{
+//     if(e.target.classList.contains('btn-kosongkan')){
+//         const dataMeja = e.target.getAttribute('data-meja');
+//         e.stopPropagation();
+//         alert(`Meja No ${dataMeja} telah dikosongkan`)
+//     }else if(e.target.closest('.kartu-meja')){
+//         const kartuMeja = e.target.closest('.kartu-meja');
+//         const noMeja = kartuMeja.getAttribute('data-meja');
+//         alert(`Melihat data meja no ${noMeja}`)
+//     }
+// })
+
+
+const pembungkusTombol = document.getElementById('pembungkus-tombol');
+
+pembungkusTombol.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('btn-kategori')){
+        const dataKategori = e.target.getAttribute('data-kategori');
+        alert(`Menampilkan menu kategori ${dataKategori}`);
+    }else if(e.target.classList.contains('btn-diskon')){
+        const dataPromo = e.target.getAttribute('data-id-promo');
         e.stopPropagation();
-        alert(`Meja No ${dataMeja} telah dikosongkan`)
-    }else if(e.target.closest('.kartu-meja')){
-        const kartuMeja = e.target.closest('.kartu-meja');
-        const noMeja = kartuMeja.getAttribute('data-meja');
-        alert(`Melihat data meja no ${noMeja}`)
+        alert(`Kode Promo ${dataPromo} Aktif`);
     }
 })
