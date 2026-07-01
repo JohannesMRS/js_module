55209e169337c7d61538da8dbecab6baee90bb68
JohannesMRS <mariojr8304@gmail.com>
2026-07-01 22:56:31 +0700

    Feat: rewrite script.js in callback folder

--- 03-asynchronous-api/01-callback/script.js
+++ 03-asynchronous-api/01-callback/script.js
@@ -3,12 +3,19 @@ const layarTimer = document.getElementById('layar-timer')
 let variabel = 0;
 
 btnSeduh.addEventListener("click", ()=>{
+    btnSeduh.disabled = true;
+    btnSeduh.style.cursor = 'not-allowed';
     let timerSeduh = setInterval(()=>{
         variabel++;
         layarTimer.textContent = variabel;
         if(variabel == 5){
             clearInterval(timerSeduh);
             alert("Kopi telah siap");
-        }
+
+                btnSeduh.disabled = false;
+                btnSeduh.style.cursor = "pointer";
+                variabel = 0;
+            }
+            layarTimer.textContent = variabel;
     }, 1000)
 })
