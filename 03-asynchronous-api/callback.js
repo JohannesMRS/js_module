6e794e53e8c98c9f82c411f3a409aa3803c05224
JohannesMRS <mariojr8304@gmail.com>
2026-07-02 21:22:49 +0700

    Feat: Rewrite callback.js

--- 03-asynchronous-api/callback.js
+++ 03-asynchronous-api/callback.js
@@ -1,13 +1,26 @@
-// console.log("Admin Menerima Data")
+// // console.log("Admin Menerima Data")
 
-// setTimeout(()=>{
-//     console.log("Data Sedang Di Proses....")
-// }, 3000)
+// // setTimeout(()=>{
+// //     console.log("Data Sedang Di Proses....")
+// // }, 3000)
 
-// console.log("Data Selesai Di Proses")
+// // console.log("Data Selesai Di Proses")
 
 
 
-setInterval(()=>{
-    console.log("Aku tak tahu apa yang terjadi")
-}, 900)
+// setInterval(()=>{
+//     console.log("Aku tak tahu apa yang terjadi")
+// }, 900)
+
+
+function fetchData(callback){
+    setTimeout(()=>{
+        const data = {nama: "Andi", role: "Developer"};
+        callback(data);
+    }, 3000);
+}
+
+
+fetchData((user)=>{
+    console.log("Data Berhasil Diambil", user);
+})
