7677d4b76fb86ed8f6d7c4db929072c541255e9e
JohannesMRS <mariojr8304@gmail.com>
2026-07-06 20:26:27 +0700

    Feat: Rewrite arrow.js

--- 03-asynchronous-api/02-function/arrow.js
+++ 03-asynchronous-api/02-function/arrow.js
@@ -6,3 +6,52 @@ const cekGenap = (x)=>{
 }
 
 console.log(cekGenap(5))
+
+
+function counter(){
+    let hitung = 0;
+
+    return function(){
+        hitung = hitung + 1;
+        return hitung;
+    }
+}
+
+const counter1 = counter();
+console.log(counter1())
+console.log(counter1())
+console.log(counter1())
+console.log(counter1())
+
+const counter2 = counter();
+console.log(counter2())
+
+
+function total(...a){
+    let hasil = 0;
+    for(let nilai of a){
+        hasil += nilai;
+    }
+    return hasil;
+}
+
+console.log(total(5, 10, 15, 20))
+
+
+function tambahLima(x){
+    return x + 5;
+}
+
+function kaliTiga(x){
+    return x * 3
+}
+
+
+function gabungkan(callBackSatu, callBackDua){
+    return function(x){
+        return callBackDua(callBackSatu(x));
+    }
+}
+
+const proses = gabungkan(tambahLima, kaliTiga);
+console.log(proses(2));
