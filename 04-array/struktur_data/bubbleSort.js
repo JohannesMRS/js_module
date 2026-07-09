dda422c0d2cc70ee0a5ad0a45c583c7074cb944b
JohannesMRS <mariojr8304@gmail.com>
2026-07-09 13:34:18 +0700

    Feat: Adding struktur_data folder in 04-array

---
+++ 04-array/struktur_data/bubbleSort.js
@@ -0,0 +1,21 @@
+let angka = [10, 5, 76, 9, 1];
+
+
+// bubble sort
+
+// for(let i = 0; i < angka.length-1; i++){
+//     for(let j = 0; j < angka.length-1-i; j++){
+//         if(angka[j] > angka[j+1]){
+//             let temp = angka[j];
+//             angka[j] = angka[j+1];
+//             angka[j+1] = temp;
+//         }
+//     }
+//     console.log(angka)
+// }
+
+
+// selection sor
+
+
+
