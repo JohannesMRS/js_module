10d2d7149d0db1304ef019677216172764e05474
JohannesMRS <mariojr8304@gmail.com>
2026-07-10 19:54:13 +0700

    Feat: Adding index.html in 01-callback folder

--- 03-asynchronous-api/01-callback/script.js
+++ 03-asynchronous-api/01-callback/script.js
@@ -1,34 +1,91 @@
-let token = ~~[Math.random() * 12345678];
-let picture = ['01.jpg'];
+// let token = ~~[Math.random() * 12345678];
+// let picture = ['01.jpg'];
 
-function login(username, callback){
-    console.log("Validasi Data.....")
-    setTimeout(()=>{
-        callback({token, username});
-    },200)
+// function login(username, callback){
+//     console.log("Validasi Data.....")
+//     setTimeout(()=>{
+//         callback({token, username});
+//     },200)
+// }
+
+// function getUser(token, callback){
+//     console.log("Validasi API Key....")
+//     if(token) setTimeout(()=>{
+//         callback({apiKey: "xKey123"});
+//     },500);
+// }
+
+// function getPicture(apiKey, callback){
+//     console.log("Menghasilkan Gambar...")
+//     if(apiKey) setTimeout(()=>{
+//         callback({pic: picture});
+//     }, 1500)
+// }
+
+// const user = login("Johannes", function(response){
+//     const {token} = response;
+//     getUser(token, function(response){
+//         const {apiKey} = response;
+//         getPicture(apiKey, function(response){
+//             const {pic} = response;
+//             console.log("Picture: ", pic);
+//         })
+//     })
+// });
+
+
+
+function prosesNama(nama, callback){
+    const hasil = nama.toUpperCase();
+    callback(hasil);
 }
 
-function getUser(token, callback){
-    console.log("Validasi API Key....")
-    if(token) setTimeout(()=>{
-        callback({apiKey: "xKey123"});
-    },500);
+prosesNama("Johannes", (hasil)=>{
+    console.log("Nama: ", hasil);
+})
+
+
+function hitungTotalBelanja(keranjang, callback){
+    let total = 0;
+
+    for(let harga of keranjang){
+        total += harga;
+    }
+
+    callback(total);
 }
 
-function getPicture(apiKey, callback){
-    console.log("Menghasilkan Gambar...")
-    if(apiKey) setTimeout(()=>{
-        callback({pic: picture});
-    }, 1500)
+const keranjang = [15000, 20000, 12000];
+hitungTotalBelanja(keranjang, (total)=>{
+    console.log(`Hasil: ${total}`);
+})
+
+
+function dataUser(userId, callback){
+    if(userId){
+        setTimeout(function(){
+            callback({id: userId, username: "dev-medan"});
+        }, 2000)
+    }
 }
 
-const user = login("Johannes", function(response){
-    const {token} = response;
-    getUser(token, function(response){
-        const {apiKey} = response;
-        getPicture(apiKey, function(response){
-            const {pic} = response;
-            console.log("Picture: ", pic);
-        })
-    })
-});
+console.log("Memulai pencarian user...")
+dataUser(101, (hasil)=>{
+    console.log("Data ditemukan: ", hasil);
+})
+
+function cekStokBarang(jumlahBeli, callback){
+    if(jumlahBeli > 10){
+        callback("Stok tidak mencukupi", null);
+    }else{
+        callback(null, "Pesanan di proses");
+    }
+}
+
+cekStokBarang(9, (err, data)=>{
+    if(err){
+        console.log("Error: ", err);
+    }else{
+        console.log("Sukses: ", data);
+    }
+})
