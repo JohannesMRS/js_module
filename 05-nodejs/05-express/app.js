e366ebaa53595af6123ee5a066bcf523954486fe
JohannesMRS <mariojr8304@gmail.com>
2026-07-18 21:14:12 +0700

    Feat: Using App Feature From Express

--- 05-nodejs/05-express/app.js
+++ 05-nodejs/05-express/app.js
@@ -1,34 +1,37 @@
-const http = require('http');
-const fs = require('fs');
-
-http
-.createServer((req, res)=>{
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-    });
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 
-    const url = req.url;
+const app = express();
+const port = 3000;
 
-    const renderHTML = (path, res)=>{
-        fs.readFile(path, (err, data)=>{
-            if(err){
-                res.write('Halaman Tidak Ditemukan');
-            }else{
-                res.write(data);
-            }
-            res.end();
-        })
-    }
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-    if(url === "/contact"){
-        renderHTML('./contact.html', res);
-    }else if(url === "/about"){
-        renderHTML('./about.html', res);
-    }else{
-        renderHTML('./index.html', res);
-    }   
-    
-}).listen(3000, ()=>{
-    console.log('Server is listening on port 3000');
+app.get('/', (req, res)=>{
+    res.sendFile('./index.html', {root: __dirname});
 })
+
+app.get('/about', (req, res)=>{
+    res.sendFile('./about.html', {root: __dirname});
+
+})
+
+app.get('/contact', (req, res)=>{
+    res.sendFile('./contact.html', {root: __dirname});
+})
+
+app.get('/product/:id', (req, res)=>{
+    res.send(`Id Produk: ${req.params.id} <br> Id Kategori: ${req.query.category}`);
+})
+
+app.use('/', (req, res)=>{
+    res.status(404);
+    res.send('404 Not Found');
+})
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
+});
+
