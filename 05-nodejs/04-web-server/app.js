406dee7b75c97ceb56258f5dffb85813f0396dd0
JohannesMRS <mariojr8304@gmail.com>
2026-07-18 14:26:54 +0700

    Feat: -----

--- 05-nodejs/04-web-server/app.js
+++ 05-nodejs/04-web-server/app.js
@@ -2,6 +2,9 @@ const http = require('http');
 
 http
 .createServer((req, res)=>{
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
     res.write('Hello World');
     res.end();
 }).listen(3000, ()=>{
