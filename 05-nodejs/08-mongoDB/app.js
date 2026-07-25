d659535577dcd2b1297db9990be05cb2ca556b5a
JohannesMRS <mariojr8304@gmail.com>
2026-07-25 21:29:35 +0700

    Feat: Insert Data To Database

--- 05-nodejs/08-mongoDB/app.js
+++ 05-nodejs/08-mongoDB/app.js
@@ -1,20 +1,21 @@
-const {MongoClient} = require('mongodb');
+import { MongoClient } from 'mongodb';
 
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
-
-const dbName = 'polmed';
-
-async function main(){
-    await client.connect();
-    console.log('Connected Successfully to server.');
-    const db = client.db(dbName);
-    const collection = db.collection('documents');
-
-    return 'done.';
-};
-
-main()
-.then(console.log)
-.catch(console.error)
-.finally(()=>client.close());
+async function runGetStarted() {
+  // Replace the uri string with your connection string
+  const uri = 'mongodb://localhost:27017';
+  const client = new MongoClient(uri);
+  try {
+    const database = client.db('polmed');
+    const mahasiswa = database.collection('mahasiswa');
+    // Queries for a movie that has a title value of 'Back to the Future'
+    await mahasiswa.insertOne({
+        nim: "2405102155",
+        nama: "Cahya Supriadi",
+        email: "cahya@gmail.com"
+    })
+    console.log(mahasiswa);
+  } finally {
+    await client.close();
+  }
+}
+runGetStarted().catch(console.dir);
