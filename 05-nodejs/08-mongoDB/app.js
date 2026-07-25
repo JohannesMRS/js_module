b9f4d164cf1dbb68d060025bf1e2fae8bd4bd7bd
JohannesMRS <mariojr8304@gmail.com>
2026-07-25 21:54:52 +0700

    Feat: Trying CRUD with mongoDB

--- 05-nodejs/08-mongoDB/app.js
+++ 05-nodejs/08-mongoDB/app.js
@@ -7,13 +7,37 @@ async function runGetStarted() {
   try {
     const database = client.db('polmed');
     const mahasiswa = database.collection('mahasiswa');
-    // Queries for a movie that has a title value of 'Back to the Future'
-    await mahasiswa.insertOne({
-        nim: "2405102155",
-        nama: "Cahya Supriadi",
-        email: "cahya@gmail.com"
-    })
-    console.log(mahasiswa);
+    // // Queries for a movie that has a title value of 'Back to the Future'
+
+    // Create Data
+    // await mahasiswa.insertOne({
+    //     nim: "2405102155",
+    //     nama: "Cahya Supriadi",
+    //     email: "cahya@gmail.com"
+    // })
+    // console.log(mahasiswa);
+
+
+    // Read Data
+    // const result = await mahasiswa.find().toArray();
+    // console.log(result);
+
+    // Update Data
+    // const filter = {nama: "Ryan Limbong"};
+    // const options = {upsert: true};
+    // const updateDoc = {
+    //     $set: {
+    //         email: "ryan25@gmail.com"
+    //     }
+    // }
+
+    // const result = await mahasiswa.updateOne(filter, updateDoc, options);
+
+    // Delete Data
+    const data = {nama: "Cahya Supriadi"};
+    const result = await mahasiswa.deleteOne(data);
+    const showRes = await mahasiswa.find().toArray();
+    console.log(showRes);
   } finally {
     await client.close();
   }
