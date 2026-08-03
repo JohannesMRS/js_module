47eaea8437a140f8162f2b98b9c921235bfdc0a4
JohannesMRS <mariojr8304@gmail.com>
2026-08-03 14:27:16 +0700

    Feat: Adding put and delete features for oxa

--- 07-api-example/01-oxa-api/routes/users.js
+++ 07-api-example/01-oxa-api/routes/users.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import Users from '../models/users.js';
 import userPayloads from '../payloads/users.js';
+import {body, validationResult} from 'express-validator';
+import bcrypt from 'bcrypt';
 
 const routeUser = express.Router();
 
@@ -16,18 +18,96 @@ routeUser.get('/', async (req, res)=>{
     }
 });
 
-routeUser.post('/', async (req, res)=>{
+routeUser.post('/', 
+    [
+        body('nama').notEmpty().withMessage('Nama Tidak Boleh Kosong'),
+        body('email').isEmail().withMessage('Sertakan Email Yang Valid'),
+        body('password').isLength({min: 8}).withMessage('Password Minimal 8 Karakter')
+    ],async (req, res)=>{
     try{
+        const errors = validationResult(req);
         const {nama, email, password} = req.body;
+        const saltRounds = 10;
+
+        // Cek Apakah Ada Error dari body yang dikirimkan
+        if(!errors.isEmpty()){
+            return userPayloads(400, errors.array(), null, res);
+        }
+
+        // Cek Duplikat Email
+        const emailDuplikat = await Users.findOne({email: email});
+        if(emailDuplikat){
+            return userPayloads(409, 'Email Sudah Dipakai', null, res);
+        }
+
+        const passwordHashed = await bcrypt.hash(password, saltRounds);
+
         const result = await Users.insertOne({
             nama,
             email,
-            password,
+            password: passwordHashed
         });
         userPayloads(200, 'Data Berhasil Ditambah', result, res);
+        
     }catch(err){
         userPayloads(500, 'Gagal Memasukkan Data', null, res);
     }
 });
+
+routeUser.put('/:id', 
+    [
+        body('nama').notEmpty().withMessage('Nama Tidak Boleh Kosong'),
+        body('email').isEmail().withMessage('Sertakan Email Yang Valid'),
+        body('password').isLength({min: 8}).withMessage('Password Minimal 8 Karakter')
+    ],async (req, res)=>{
+    try{
+        const errors = validationResult(req);
+        const {nama, email, password} = req.body;
+        const saltRounds = 10;
+
+        // Cek Apakah Ada Error dari body yang dikirimkan
+        if(!errors.isEmpty()){
+            return userPayloads(400, errors.array(), null, res);
+        }
+
+        // Cek Duplikat Email
+        const emailDuplikat = await Users.findOne(
+            {
+                email: email,
+                _id: { $ne: req.params.id}
+            }
+        );
+        if(emailDuplikat){
+            return userPayloads(409, 'Email Sudah Dipakai', null, res);
+        }
+
+        const passwordHashed = await bcrypt.hash(password, saltRounds);
+
+        const result = await Users.findOneAndUpdate(
+            {_id:req.params.id},
+            {$set: {
+                nama,
+                email,
+                password: passwordHashed
+            }},
+            {new: true, runValidators: true}
+        );
+
+        const showData = await Users.findOne({_id:req.params.id});
+        userPayloads(200, 'Data Berhasil DiUpdate', result, res);
+        
+    }catch(err){
+        userPayloads(500, 'Gagal Memasukkan Data', null, res);
+    }
+});
+
+routeUser.delete('/:id', async (req, res)=>{
+   try{
+    const result = await Users.findOneAndDelete({_id: req.params.id});
+    userPayloads(200, 'Data Berhasil Dihapus', result, res);    
+   }catch(err){
+    userPayloads(500, 'Gagal Menghapus Data', null, res);
+   }
+})
 
 export default routeUser;
