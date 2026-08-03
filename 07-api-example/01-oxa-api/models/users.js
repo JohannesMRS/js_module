fec103923691937c8c020055d84331774ec07491
JohannesMRS <mariojr8304@gmail.com>
2026-08-03 12:42:36 +0700

    Feat: Adding post feature for oxa

--- 07-api-example/01-oxa-api/models/users.js
+++ 07-api-example/01-oxa-api/models/users.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     nama: {type: String, required: true},
     email: {type: String, required: true},
+    password: {type: String, required: true}
 });
 
 export default mongoose.model('Users', userSchema, 'users');
--- 07-api-example/01-oxa-api/routes/users.js
+++ 07-api-example/01-oxa-api/routes/users.js
@@ -6,18 +6,27 @@ const routeUser = express.Router();
 
 routeUser.get('/', async (req, res)=>{
     try{
-        const queryParams = String(req.query.search || ''); // mengindari NoSQL Injection
-        const result = await Users.find({nama: queryParams}).select('nama -_id');
-
+        const result = await Users.find();
         if(result.length === 0){
             return userPayloads(404, 'Data Tidak Ditemukan', [], res);
         }
-
         userPayloads(200, 'Data Mahasiswa', result, res);
-
-        
     }catch(err){
         userPayloads(500, 'Gagal Mengambil Data', null, res);
+    }
+});
+
+routeUser.post('/', async (req, res)=>{
+    try{
+        const {nama, email, password} = req.body;
+        const result = await Users.insertOne({
+            nama,
+            email,
+            password,
+        });
+        userPayloads(200, 'Data Berhasil Ditambah', result, res);
+    }catch(err){
+        userPayloads(500, 'Gagal Memasukkan Data', null, res);
     }
 });
 
