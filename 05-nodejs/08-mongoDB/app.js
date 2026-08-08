278539406ce1cb4371137ba6ed9dec9e6dc07582
JohannesMRS <mariojr8304@gmail.com>
2026-08-08 18:53:04 +0700

    Feat: Adding mongoose and nodemon in 08-mongoDB folder

--- 05-nodejs/08-mongoDB/app.js
+++ 05-nodejs/08-mongoDB/app.js
@@ -1,45 +1,12 @@
-import { MongoClient } from 'mongodb';
+import mongoose from "mongoose";
 
-async function runGetStarted() {
-  // Replace the uri string with your connection string
-  const uri = 'mongodb://localhost:27017';
-  const client = new MongoClient(uri);
-  try {
-    const database = client.db('polmed');
-    const mahasiswa = database.collection('mahasiswa');
-    // // Queries for a movie that has a title value of 'Back to the Future'
-
-    // Create Data
-    // await mahasiswa.insertOne({
-    //     nim: "2405102155",
-    //     nama: "Cahya Supriadi",
-    //     email: "cahya@gmail.com"
-    // })
-    // console.log(mahasiswa);
-
-
-    // Read Data
-    // const result = await mahasiswa.find().toArray();
-    // console.log(result);
-
-    // Update Data
-    // const filter = {nama: "Ryan Limbong"};
-    // const options = {upsert: true};
-    // const updateDoc = {
-    //     $set: {
-    //         email: "ryan25@gmail.com"
-    //     }
-    // }
-
-    // const result = await mahasiswa.updateOne(filter, updateDoc, options);
-
-    // Delete Data
-    const data = {nama: "Cahya Supriadi"};
-    const result = await mahasiswa.deleteOne(data);
-    const showRes = await mahasiswa.find().toArray();
-    console.log(showRes);
-  } finally {
-    await client.close();
+const conn = async()=>{
+  try{
+    await mongoose.connect('mongodb://localhost:27017/oxa');
+    console.log('Koneksi Berhasil');
+  }catch(err){
+    console.error(err);
   }
 }
-runGetStarted().catch(console.dir);
+
+conn();
